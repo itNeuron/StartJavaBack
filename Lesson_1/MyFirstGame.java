6b8aa7e2b69d749329f74fb5fdbcc672dc665300
Romanov Anrdey <r200r1988@gmail.com>
2022-01-28 22:42:43 +0400

    Поправил Unicode, MyFirstGame добавил изменение переменной

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,15 +1,18 @@
 public class MyFirstGame {
 
     public static void main(String[] args) {
-        int numUser = 52;
-        int numMem = 52;
+        int playerNum = 80;
+        for (int i = 80; i >= 40; i--) {
+            playerNum--;
+        }
+        int hiddenNum = 53;
 
-        if (numUser < numMem) {
-            System.out.println("Данное число меньше того, что загадал компьютер!");
-        } else if (numUser > numMem) {
-            System.out.println("Данное число больше того, что загадал компьютер!");
-        } else if (numUser == numMem) {
-            System.out.println("Поздравляю, число угадано!");
+        if (playerNum < hiddenNum) {
+            System.out.println(playerNum + " Данное число меньше того, что загадал компьютер!");
+        } else if (playerNum > hiddenNum) {
+            System.out.println(playerNum + " Данное число больше того, что загадал компьютер!");
+        } else if (playerNum == hiddenNum) {
+            System.out.println(playerNum + " Поздравляю, число угадано!");
         }
     }
 }
--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -2,8 +2,7 @@ public class Unicode {
 
     public static void main(String[] args) {
         for (int i = 9398; i <= 10178; i++) {
-            char a = (char) i;
-            System.out.print(a + " ");
+            System.out.print((char) i);
         }
     }
 }
