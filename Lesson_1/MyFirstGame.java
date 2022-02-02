baca54284d2d8854aab283b962845e526b31dd10
Romanov Anrdey <r200r1988@gmail.com>
2022-02-02 21:57:35 +0400

    Изменил MyFirstGame

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -2,24 +2,19 @@ public class MyFirstGame {
 
     public static void main(String[] args) {
         int playerNum = 35;
-
-        int b = 5;
-        int hiddenNum = 0;
-        do {
-            if (b % 2 != 0) {
-                hiddenNum += b;
+        int hiddenNum = 6;
+        while (playerNum != hiddenNum) {
+            if (playerNum > hiddenNum) {
+                System.out.println(playerNum + " Данное число больше того, что загадал компьютер!");
+                System.out.println("   Попробуйте ещё раз!");
+                playerNum--;
             }
-            b++;
-        } while (b < 13);
-        hiddenNum -= 3;
-        if (playerNum == hiddenNum) {
-            System.out.println(playerNum + " Поздравляю, число угадано!");
-        } else if (playerNum > hiddenNum) {
-            System.out.println(playerNum + " Данное число больше того, что загадал компьютер!");
-            System.out.println("   Попробуйте ещё раз!");
-        } else if (playerNum < hiddenNum) {
-            System.out.println(playerNum + " Данное число меньше того, что загадал компьютер!");
-            System.out.println("   Попробуйте ещё раз!");
+            else if (playerNum < hiddenNum) {
+                System.out.println(playerNum + " Данное число меньше того, что загадал компьютер!");
+                System.out.println("   Попробуйте ещё раз!");
+                playerNum++;
+            }
         }
+        System.out.println("Число угаданно. Победа!");
     }
 }
