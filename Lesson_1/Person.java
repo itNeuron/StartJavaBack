5e44266eba3d683755e74138e3713610ad076254
Romanov Anrdey <r200r1988@gmail.com>
2022-02-09 22:02:12 +0400

    Person

---
+++ Lesson_1/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+
+        int growth = 177;
+        int weight = 69;
+        int age = 33;
+
+        void walk() {
+        }
+        boolean move() {
+        }
+        String talk() {
+        }
+}
