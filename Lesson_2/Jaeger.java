4436ceb7e1fd3154ff15781e10e01419a60cb9ac
Romanov Anrdey <r200r1988@gmail.com>
2022-03-07 04:30:49 +0500

    JaegerTest

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,74 @@
+public class Jaeger {
+
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int speed;
+    private int strength;
+    private int armor;
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public boolean drift() {
+        System.out.println("Вы вошли в дрифт!");
+        return true;
+    }
+
+    public void move() {
+        System.out.println("Он прошёл 5 шагов!");
+    }
+
+    public String scanKaiju() {
+        return "Now othing.";
+    }
+
+    public void useAbility() {
+        System.out.println("He Attak!!");
+    }
+
+    @Override
+    public String toString() {
+        return "Jaeger{" +
+                "modelName='" + modelName + '\'' +
+                ", mark='" + mark + '\'' +
+                ", origin='" + origin + '\'' +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", speed=" + speed +
+                ", strength=" + strength +
+                ", armor=" + armor +
+                '}';
+    }
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,39 @@
+public class JaegerTest {
+
+    public static void main(String[] args) {
+        Jaeger jaegerOne = new Jaeger();
+
+        jaegerOne.setModelName("Gipsy Danger");
+        jaegerOne.setMark("Mark 6");
+        jaegerOne.setOrigin("USA");
+        jaegerOne.setHeight(79.25f);
+        jaegerOne.setWeight(1.98f);
+        jaegerOne.setSpeed(7);
+        jaegerOne.setStrength(8);
+        jaegerOne.setArmor(6);
+
+        Jaeger сoyoteTango = new Jaeger();
+
+        сoyoteTango.setModelName("Coyote Tango");
+        сoyoteTango.setMark("Mark 1");
+        сoyoteTango.setOrigin("Japan");
+        сoyoteTango.setHeight(85.34f);
+        сoyoteTango.setWeight(2.312f);
+        сoyoteTango.setSpeed(5);
+        сoyoteTango.setStrength(7);
+        сoyoteTango.setArmor(4);
+
+        System.out.println(jaegerOne);
+        System.out.println(сoyoteTango);
+
+        System.out.println(jaegerOne.drift());
+        jaegerOne.move();
+        System.out.println(jaegerOne.scanKaiju());
+        jaegerOne.useAbility();
+
+        System.out.println(сoyoteTango.drift());
+        jaegerOne.move();
+        System.out.println(сoyoteTango.scanKaiju());
+        jaegerOne.useAbility();
+    }
+}
