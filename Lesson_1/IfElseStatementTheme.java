d9a3b34eaa765f5fb5507f5f8efdd5891f2ee3ef
Романов Андрей <r200r1988@gmail.com>
2023-01-05 23:14:48 +0500

    Добавление файлов

---
+++ Lesson_1/IfElseStatementTheme.java
@@ -0,0 +1,257 @@
+package StartJava.Lesson_1;
+
+public class IfElseStatementTheme {
+    public static void main(String[] args) {
+        //TODO: 29.08.2022 1. Перевод псевдокода на язык Java (это название задачи)
+        //● перепишите псевдокод, приведенный ниже, с помощью Java
+        //● используйте переменные, условные операторы, логическое НЕ
+        //● придумайте сообщения и отобразите в консоли
+        System.out.println("\nЗадание №1");
+        int alt = 34;
+        if (alt > 20) {
+            System.out.println("Тебе повезло");
+        } else {
+            System.out.println("Есть куда рости!");
+        }
+
+        boolean isMaleGender = true;
+        if (!isMaleGender) {
+            System.out.println("Здоровья тебе!");
+        }
+        if (isMaleGender) {
+            System.out.println("На тебе больше ответственности!");
+        }
+
+        float height = 1.76f;
+        if (height < 1.80f) {
+            System.out.println("Средний рост!");
+        } else {
+            System.out.println("Иногда в автобусах неудобно сидеть!");
+        }
+
+        char firstLetterName = "A".charAt(0);
+        if (firstLetterName == 'M') {
+            System.out.println("Как название остановки метро");
+        } else if (firstLetterName == 'I') {
+            System.out.println("Ингвар? :)");
+        } else {
+            System.out.println("С неё начинается алфавит..");
+        }
+
+        // TODO: 30.08.2022 2. Поиск max и min числа
+        //○ создайте две переменные, присвоив им любые числа
+        //○ найдите максимальное и минимальное число
+        //○ выведите числа на консоль, указав какое из них max и min
+        System.out.println("\nЗадание №2");
+        int a = 5;
+        int b = 2;
+        if (a>b) {
+            System.out.println(a + " max число");
+            System.out.println(b + " min число");
+        } else {
+            System.out.println(b + " max число");
+            System.out.println(a + " min число");
+        }
+
+        // TODO: 31.08.2022 3. Работа с числом
+        //○ создайте переменную с любым числом
+        //○ определите, являться ли оно:
+        //i. нулем
+        //ii. четным (нечетным)
+        //iii. отрицательным (положительным)
+        //○ если число равно 0, то остальные проверки не должны
+        //осуществляться
+        //○ отобразите полученные результаты
+        System.out.println("\nЗадание №3");
+        int a3 = -1;
+        if (a3 == 0) {
+            System.out.println("Число a3 равно 0");
+        } else  {
+            System.out.println("Число a3 не равно 0");
+            if ((a3 % 2) == 0) {
+                System.out.println("Число a3 чётное");
+            } else {
+                System.out.println("Число a3 не чётное");
+            }
+            if (a3 > 0) {
+                System.out.println("Число a3 больше 0");
+            } else {
+                System.out.println("Число a3 меньше 0");
+            }
+        }
+
+        // TODO: 01.09.2022 4. Поиск одинаковых цифр в числах
+        //○ создайте две переменные, присвоив им два трехзначных целых
+        //числа
+        //○ найдите в них одинаковые цифры, стоящие в одном и том же
+        //разряде
+        //○ отобразите результат в формате:
+        //i. в числах N и M одинаковые цифры A, B, C стоят в X, Y, Z
+        //разрядах
+        System.out.println("\nЗадание №4");
+        int N = 326;
+        int M = 318;
+        int N1 = N / 100;
+        int M1 = M / 100;
+        int N2 = N % 100;
+        int M2 = M % 100;
+        int N11 = N2 / 10;
+        int M11 = M2 / 10;
+        int N111 = N % 10;
+        int M111 = M % 10;
+        if (N1 == M1 & N11 == M11 & N111==M111) {
+            System.out.println("В числах " + N + " и " + M + " одинаковые цифры " + N1 + " и " + N11 + "" + N111 + " стоят в 1,2,3 разрядах");
+        } else if (N1 == M1 & N11 == M11) {
+            System.out.println("В числах " + N + " и " + M + " одинаковые цифры " + N1 + " и " + N11 + " стоят в 1,2 разрядах");
+        } else if (N1 == M1 & N111 == M111) {
+            System.out.println("В числах " + N + " и " + M + " динаковые цифры " + N1 + " и " + N111 + " стоят в 1,3 разрядах");
+        } else if (N11 == M11 & N111 == M111) {
+            System.out.println("В числах " + N + " и " + M + " одинаковые цифры " + N11 + " и " + N111 + " стоят в 2,3 разрядах");
+        }  else if (N1 == M1) {
+            System.out.println("В числах " + N + " и " + M + " одинаковые цифры " + N1 + " стоят в 1 разряде");
+        }  else if (N11 == M11) {
+            System.out.println("В числах " + N + " и " + M + " одинаковые цифры " + N11 + " стоят в 2 разряде");
+        }  else if (N111 == M111) {
+            System.out.println("В числах " + N + " и " + M + " одинаковые цифры " + N111 + " стоят в 3 разряде");
+        }
+
+        // TODO: 03.09.2022 5. Определение буквы, числа или символа по их коду
+        //○ создайте переменную типа char
+        //○ присвойте ей значение ‘\u0057’
+        //○ определите — это код буквы, числа или другого символа
+        //○ использовать для этого методы классов Java запрещено!
+        //○ выведите в консоль сам символ и информацию о нем:
+        //i. маленькая (большая) буква
+        //ii. это число
+        //iii. не буква и не число
+        System.out.println("\nЗадание №5");
+        char a5 = '\u0057';
+        System.out.print((int) a5);
+
+
+
+
+        // TODO: 03.09.2022 6. Определение суммы вклада и начисленных банком %
+        //○ сумма вклада равна 300_000 руб.
+        //○ если она:
+        //i. меньше 100 000, то банк начисляет 5% годовых
+        //ii. от 100 000 до 300 000, то — 7%
+        //iii. больше 300 000, то — 10%
+        //○ отобразите в консоли:
+        //i. сумму вклада
+        //ii. начисленный %
+        //iii. итоговую сумму с %
+        System.out.println("\nЗадание №6");
+        int a6 = 300000;
+        int a66 = (int) (a6*0.05);
+        if (a6 < 100000) {
+            System.out.println("Сумма вклада - " + a6);
+            System.out.println("Начисленный процент - " + (int) (a6*0.05));
+            System.out.println("Итоговая сумму с % " + (a6 + (int) (a6*0.05)));
+        } else if (100000 < a6 | a6 < 300000) {
+            System.out.println("Сумма вклада - " + a6);
+            System.out.println("Начисленный процент - " + ((int) (a6*0.07)));
+            System.out.println(a6 + (int) (a6*0.07));
+        } else if (a6 > 300000) {
+            System.out.println("Сумма вклада - " + a6);
+            System.out.println("Начисленный процент - " + (int)(a6 * 0.1));
+            System.out.println("Итоговая сумму с % " + a6 + (int)(a6 * 0.1));
+        }
+
+        // TODO: 03.09.2022 7. Определение оценки по предметам
+        //○ студент получил итоговые % по предметам:
+        //i. история 59%
+        //ii. программирование 91%
+        //○ определите оценки по каждому предмету:
+        //i. <= 60% — 2
+        //ii. > 60% - 3
+        //iii. > 73% - 4
+        //iv. > 91% - 5
+        //○ выведите в консоль:
+        //i. оценку и напротив предмет
+        //ii. средний балл оценок по предметам
+        //iii. средний % по предметам
+        System.out.println("\nЗадание №7");
+        int histori = 59;
+        int programming = 91;
+        System.out.println(histori);
+        System.out.println(programming);
+        if (histori <= 60) {
+            System.out.println("2 - " + "история");
+        } else if (histori > 60 & histori < 73) {
+            System.out.println("3 - " + "история");
+        } else if (histori > 73 & histori <91) {
+            System.out.println("4 - " + "история");
+        } else if (histori > 91) {
+            System.out.println("5 - история");
+        }
+        if (programming <= 60) {
+            System.out.println("2 - " + "программирование");
+        } else if (programming > 60 & programming < 73) {
+            System.out.println("3 - " + "программирование");
+        } else if (programming > 73 & programming < 91) {
+            System.out.println("4 - " + "программирование");
+        } else if (programming > 91) {
+            System.out.println("5 - программирование");
+        }
+        if (histori <= 60 & programming <= 60) {
+            System.out.println("Средний бал 2");
+        } else if (histori <= 60 & programming > 60 & programming < 73 | programming <= 60 & histori > 60 & histori < 73) {
+            System.out.println("Средний бал 2.5");
+        } else if (histori > 60 & histori < 73 & programming > 60 & programming < 73) {
+            System.out.println("Средний бал 3");
+        } else if (histori > 60 & histori < 73 & programming > 73 & programming < 91 | histori > 73 & histori < 91 & programming > 60 & programming < 73) {
+            System.out.println("Средний бал 3.5");
+        } else if (histori > 73 & histori < 91 & programming > 73 & programming < 91) {
+            System.out.println("Средний бал 4");
+        } else if (histori > 73 & histori < 91 & programming > 91 | histori > 91 & programming > 73 & programming < 91) {
+            System.out.println("Средний бал 4.5");
+        } else if (histori > 91 & programming > 91) {
+            System.out.println("Средний бал 5");
+        }
+        System.out.println("Средний % по предметам - " + ((histori + programming) /2));
+
+        // TODO: 03.09.2022 8. Расчет прибыли
+        //○ продавец снимает помещение за 5 000 руб. в месяц
+        //○ в среднем в месяц он продает товара на 13 000 руб.
+        //○ при этом его себестоимость равна 9 000 руб.
+        //○ подсчитайте годовую прибыль
+        //○ отобразите ее в консоли в следующем формате, например:
+        //i. прибыль за год: -1000 руб.
+        //ii. прибыль за год: +3000 руб.
+        //○ если прибыль положительная, то рядом с числом необходимо
+        //отображать +
+        System.out.println("\nЗадание №8");
+        int arenda = 5000;
+        int viruchka = 13000;
+        int sebes = 9000;
+        int prib= viruchka - arenda - sebes;
+        int pribGod = prib * 12;
+        System.out.println("прибыль за год:" + pribGod);
+
+        // TODO: 03.09.2022 9. Подсчет количества банкнот
+        //○ из банкомата вам необходимо снять 567 USD
+        //○ подсчитайте, с помощью какого количества банкнот номиналом 1,
+        //10, 100 банкомат может выдать необходимую сумму
+        //○ при этом в банкомате осталось только 5 банкнот номиналом 10
+        //○ проведите обратный расчет начальной суммы по полученному
+        //количеству банкнот, чтобы убедиться в правильности вычислений
+        //○ если каких-то банкнот не хватает, то их нужно заменить
+        //банкнотами другого номинала
+        //○ выведите в консоль:
+        //i. номиналы банкнот
+        //ii. требуемое их количество
+        //iii. посчитанную исходную сумму
+        System.out.println("\nЗадание №9");
+        int a9 = 567;
+        int sotni = a9 /100;
+        int c9 = a9 % 100;
+        int desjatki = (c9 / 10)-1;
+        int edinizi = (c9 % 10) + 10;
+        System.out.println("Сотни - " + sotni);
+        System.out.println("Десятки - " + desjatki);
+        System.out.println("Единицы - " + edinizi);
+        int p9 = sotni *100 + desjatki * 10 + edinizi;
+        System.out.println(p9);
+    }
+}
---
+++ Lesson_1/VariablesTheme.java
@@ -0,0 +1,239 @@
+package StartJava.Lesson_1;
+
+public class VariablesTheme {
+    public static void main(String[] args) {
+        // TODO: 15.08.2022 1. Создание переменных и вывод их значений на консоль (это название задачи)
+        //● объявите переменные всех существующих в Java примитивных
+        //типов данных, присвоив им значения
+        //● в качестве значений используйте информацию о своем
+        //компьютере: количество ядер, частота процессора и тд.
+        //● выведите эти данные на консоль
+        System.out.println("\nЗадание №1");
+        byte pcCore = 6;
+        short c = 700;
+        int memary = 756;
+        long frequency = 3200l;
+        float d = 5512.389887f;
+        double f = 2888888888.333555d;
+        char e = 'a';
+        boolean lovePc = true;
+        System.out.println(" pcCorea = " + pcCore);
+        System.out.println(" memary = " + memary);
+        System.out.println(" frequency = " + frequency);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println("не понятное число - " + f);
+        System.out.println(e);
+        System.out.println(" lovePc = " + lovePc);
+
+        // TODO: 15.08.2022 2. Расчет стоимости товара со скидкой
+        //○ товар X стоит 100 руб, а товар Y — 200 руб.
+        //○ вместе на них действует скидка 11%
+        //○ отобразите в консоли:
+        //i. сумму скидки
+        //ii. общую стоимость товаров со скидкой
+
+        System.out.println("\nЗадание №2");
+        int saks = 100;
+        int shor = 200;
+        double sale = 0.11;
+        double resul = (saks + shor) * sale;
+        System.out.println(resul);
+        System.out.println(saks + shor - resul);
+
+        // TODO: 15.08.2022 3. Вывод на консоль слова JAVA
+        //Отобразите в консоли слово JAVA, как в примере ниже:
+        //J a v v a
+        //J a a v v a a
+        //J J aaaaa V V aaaaa
+        //JJ a a V a a
+        //4.
+
+        System.out.println("\nЗадание №3");
+        System.out.println();
+        System.out.println("    J     a   v     v   a");
+        System.out.println("    J    a a   v   v   a a");
+        System.out.println("J   J   aaaaa   V V   aaaaa");
+        System.out.println(" J J   a     a   V   a     a");
+
+        // TODO: 15.08.2022 4. Отображение min и max значений числовых типов данных
+        //● создайте переменные всех целых числовых типов
+        //● присвойте им самые большие числа, которые они могут хранить
+        //● инкрементируйте, а затем декрементируйте все значения на
+        //единицу
+        //● выводите данные на консоль для каждой переменной в
+        //следующей последовательности:
+        //○ первоначальное значение
+        //○ значение после инкремента
+        //○ значение после декремента
+        System.out.println("\nЗадание №4");
+        byte g = 127;
+        short k = 32_767;
+        int h = 2_147_483_647;
+        long i = 9_223_372_036_854_775_807l;
+        float l = 3.1455555f;
+        double n = 3.1456552889656326d;
+        System.out.println(g);
+        System.out.println(g++);
+        System.out.println(g--);
+        System.out.println(k);
+        System.out.println(k++);
+        System.out.println(k--);
+        System.out.println(h);
+        System.out.println(h++);
+        System.out.println(h--);
+        System.out.println(i);
+        System.out.println(i++);
+        System.out.println(i--);
+        System.out.println(l);
+        System.out.println(l++);
+        System.out.println(l--);
+        System.out.println(n);
+        System.out.println(n++);
+        System.out.println(n--);
+
+        // TODO: 16.08.2022 5. !!!!! Перестановка значений переменных
+        //○ создайте две переменные типа double, присвоив им значения 1.1 и
+        //2.2
+        //○ поменяйте значения переменных местами разными способами:
+        //i. с помощью третьей переменной
+        //ii. с помощью арифметических операций
+        //iii. с помощью побитовой операции ^ (при этом дробная часть
+        //не должна теряться)
+        //○ перед началом очередных перестановок присваивать переменным
+        //исходные значения не нужно
+        //○ отобразите в консоли:
+        //i. информацию о применяемом способе перестановки
+        //значений
+        //ii. исходные значения переменных
+        //iii. новые значения переменных
+        System.out.println("\nЗадание №5");
+        double one = 1.1;
+        double two = 2.2;
+
+        System.out.println("с помощью третьей переменной");
+        double three = -1.1;
+        System.out.println("Исходное число :" + one);
+        System.out.println("Исходное число :" + two);
+        System.out.println(one = one - three);
+        System.out.println(two = two + three);
+
+        System.out.println("с помощью арифметических операций");
+        System.out.println("Исходное число :" + one);
+        System.out.println("Исходное число :" + two);
+        System.out.println(one = one - two);
+        System.out.println(two = two + one);
+
+
+        System.out.println("с помощью побитовой операции ^");
+        System.out.println("Исходное число: " + one);
+        System.out.println("Исходное число: " + two);
+
+        int one1 = (int) ( one );
+        int two1 = (int) ( two );
+        int de = one1 ^ 11;
+        int de1 = two1 ^ 11;
+        System.out.println(Integer.toBinaryString(two1^10));
+        //System.out.println(Integer.toBinaryString(ne));
+        //System.out.println(de1);
+        //System.out.println(ne);
+        float re1 = (float) (one % 1);
+        float ne1 = (float) ( two % 2);
+        System.out.println("тренировка " + re1);
+        System.out.println("тренировка " + ne1);
+        //2-10===1-01
+        //перевести цифры в десятичные, отзеркалить их, перевести  в десятичные, прибавить десятые доли
+
+
+
+
+
+        // TODO: 16.08.2022 6.Вывод символов и их кодов
+        //○ создайте 5 переменных, присвоив им значения: 35, 38, 64, 94, 95
+        //○ отобразите в консоли:
+        //i. код символа
+        //ii. напротив кода соответствующий ему символ (делайте это программно, а не написав его самим) из ASCII-таблицы
+        System.out.println("\nЗадание №6");
+        int digit1 = 35;
+        int digit2 = 38;
+        int digit3 = 64;
+        int digit4 = 94;
+        int digit5 = 95;
+       // System.out.println((char) dig1 + перевод символа в ASCII);
+        System.out.println((char) digit1);
+        System.out.println((char) digit2);
+        System.out.println((char) digit3);
+        System.out.println((char) digit4);
+        System.out.println((char) digit5);
+
+        // TODO: 22.08.2022 7. Произведение и сумма цифр числа
+        //○ имеется число 345
+        //○ найдите произведение, а затем сумму его цифр
+        //○ не используйте цикл
+        //○ выделяйте каждую цифру числа программно, а не написав их самим
+        //○ используйте для этого только операции / и %
+        //○ выведите результат в следующем формате:
+        //i. сумма цифр числа N = столько-то
+        //ii. произведение цифр числа N = столько-то
+
+        System.out.println("\nЗадание №7");
+        int a = 345;
+        int b5 = a / 100;
+        int c5 = a % 300;
+        int c6 = c5 /10;
+        int d5 = a % 34;
+        int e1 = b5 + c6 + d5;
+        int f1 = b5 * c6 * d5;
+        System.out.println("Сумма цифр числа 345 = " + e1);
+        System.out.println("Произведение цифр числа 345 = " + f1);
+
+        // TODO: 22.08.2022 8.Вывод на консоль ASCII-арт Дюка
+        //○ отобразите в консоли Java-талисман, используя символы из
+        System.out.println("\nЗадание №8");
+        //примера ниже:
+        int q1 = 47;
+        int q2 = 92;
+        int q3 = 95;
+        int q4 = 40;
+        int q5 = 41;
+        System.out.println("     " + (char)q1 + " " + (char)q2 + "    ");
+        System.out.println("    " + (char)q1 +"   " + (char)q2 + "   ");
+        System.out.println("   " + (char)q1 + (char)q3 + (char)q4 + "  " + (char)q5 + (char)q2);
+        System.out.println("  " + (char)q1 + "       " + (char)q2);
+        System.out.println(" " + (char)q1 + "____" + (char)q3 +  (char)q1 + (char)q2 + "__"  + (char)q2);
+
+        // TODO: 22.08.2022 9.	Отображение количества сотен, десятков и единиц числа
+        //○	имеется число 123
+        //○	выделите у него программно сотни, десятки и единицы
+        //○	используйте для этого только операции / и %
+        //○	отобразите результат, как “Число N содержит:”
+        //i.	X сотен
+        //ii.	Y десятков
+        //iii.	Z единиц
+        System.out.println("\nЗадание №9");
+        int a3 = 123;
+        int b3 = a3 /100;
+        int c3 = a3 % 100;
+        int d3 = c3 / 10;
+        int e3 = a3 % 12;
+        if (b3 > 2) {
+            System.out.println(b3 + " сотен");
+        } else {
+            System.out.println(b3 + " сотня");
+        }
+        System.out.println(d3 + " десятков");
+        System.out.println(e3 + " единиц");
+
+        // TODO: 22.08.2022 10.	Преобразование секунд
+        //○	имеется количество секунд 86399
+        //○	переведите его в часы, минуты и секунды
+        //○	используйте для этого только операции / и %
+        //○	отобразите полученные значения в формате ЧЧ:ММ:СС
+        System.out.println("\nЗадание №10");
+        float w1 = 86_399;
+        int w2 = (int) (w1 / 3600);
+        float w3 = (w1 / 3600) % 23 * 60;
+        float w4 = (w1 / 3600) % 23 * 60 % 59*60;
+        System.out.println(w2 + ":" + (int) w3 + ":"  + (int) w4);
+    }
+}
