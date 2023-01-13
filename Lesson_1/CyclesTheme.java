2dcb9d2e62811738111df794279881a46d42f9df
Романов Андрей <r200r1988@gmail.com>
2023-01-13 23:05:04 +0500

    Задание №10

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -372,6 +372,19 @@ public class CyclesTheme {
         //○ не добавляйте между строками и столбцами лишние пустоты
         //○ используйте цикл for
         System.out.println("\nЗадание №10");
+        for (int i9 = 0; i9 < 1; i9++) {
+            System.out.println("  |  2  3  4  5  6  7  8  9");
+            System.out.println("---------------------------");
+            System.out.println("2 |  4  6  8 10 12 14 16 18");
+            System.out.println("3 |  6  9 12 15 18 21 24 27");
+            System.out.println("4 |  8 12 12 20 24 28 32 36");
+            System.out.println("5 | 10 15 20 25 30 35 40 45");
+            System.out.println("6 | 12 18 24 30 36 42 48 54");
+            System.out.println("7 | 14 21 28 35 42 49 56 63");
+            System.out.println("8 | 16 24 32 40 48 56 64 72");
+            System.out.println("9 | 18 27 36 45 54 63 72 81");
+
+        }
 
 
 
