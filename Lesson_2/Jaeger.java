8cd5c11853cc90d92a19b764e195e9904fb71d69
Романов Андрей <r200r1988@gmail.com>
2023-01-31 03:43:18 +0500

    добавлены конструкторы

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -11,6 +11,20 @@ public class Jaeger {
     private int strength;
     private int armor;
 
+    public Jaeger() {
+        
+    }
+    public Jaeger(String modelName, String mark, String origin, float height, float weight, int speed, int strength, int armor) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+        this.speed = speed;
+        this.strength = strength;
+        this.armor = armor;
+    }
+
     public void setModelName(String modelName) {
         this.modelName = modelName;
     }
